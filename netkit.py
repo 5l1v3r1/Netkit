08f08b86248f409ea8dfd586d86e0ef53f0a6dc7
Fzin <58658417+Fzinxl@users.noreply.github.com>
2020-09-23 16:32:59 -0300

    Add files via upload

--- netkit.py
+++ netkit.py
@@ -55,83 +55,81 @@ if '-e' in argv:
 
 ########################################################################
 
-for i in argv:
+if '-l' in argv:
+	try:
+		args['listen'] = int(argv[(argv.index('-l')+1)])
+	except KeyError and IndexError:
+		print('Netkit: missing arguments')
+		exit()
+	except ValueError:
+		print("Netkit: invalid argument.")
+		exit()
 
-	if i == '-l' and args == {}:
-		try:
-			args['listen'] = int(argv[(argv.index('-l')+1)])
-		except KeyError and IndexError:
-			print('Netkit: missing arguments')
-			exit()
-		except ValueError:
-			print("Netkit: invalid argument.")
-			exit()
-
-		from modules.modules import bind_tcp
+	from modules.modules import bind_tcp
 
 
-		if exe != False:
-			bind_tcp("0.0.0.0", args['listen'], exe, max=max)
-		else:
-			bind_tcp("0.0.0.0", args['listen'], max=max)
+	if exe != False:
+		bind_tcp("0.0.0.0", args['listen'], exe, max=max)
+	else:
+		bind_tcp("0.0.0.0", args['listen'], max=max)
 
 
 ###########################################################################
 
 
-	elif i == '-c' and args == {}:
-		try:
-			args['host_c'] = argv[(argv.index('-c')+1)]
-			args['port_c'] = int(argv[(argv.index(args['host_c'])+1)])
-		except KeyError and IndexError:
-			print('Netkit: missing arguments')
-			exit()
-		except ValueError:
-			print("Netkit: invalid argument.")
-			exit()
-		from modules.modules import connect_tcp
-		try:
-			if exe != False:
-				connect_tcp(args['host_c'], args['port_c'], exe)
-			else:
-				connect_tcp(args['host_c'], args['port_c'])
-		except ConnectionRefusedError:
-			print('Netkit: Connection Refused')
-			pass
-		except:
-			error()
+elif '-c' in argv:
+	try:
+		args['host_c'] = argv[(argv.index('-c')+1)]
+		args['port_c'] = int(argv[(argv.index(args['host_c'])+1)])
+	except KeyError and IndexError:
+		print('Netkit: missing arguments')
+		exit()
+	except ValueError:
+		print("Netkit: invalid argument.")
+		exit()
+	from modules.modules import connect_tcp
+	try:
+		if exe != False:
+			connect_tcp(args['host_c'], args['port_c'], exe)
+		else:
+			connect_tcp(args['host_c'], args['port_c'])
+	except ConnectionRefusedError:
+		print('Netkit: Connection Refused')
+		pass
+	except:
+		error()
 
 
 #############################################################################
 
 
-	elif i == '--dnsr':
-		try:
-			domain = argv[(argv.index('--dnsr')+1)]
-		except IndexError:
-			print('Netkit: missing arguments')
-			exit()
-		from modules.modules import dns_resolver
-		try:
-			dns_resolver(domain)
-		except NameError:
-			pass
+elif '--dnsr' in argv:
+	try:
+		domain = argv[(argv.index('--dnsr')+1)]
+	except IndexError:
+		print('Netkit: missing arguments')
+		exit()
+	from modules.modules import dns_resolver
+	try:
+		dns_resolver(domain)
+	except NameError:
+		pass
 
 
 ###############################################################################
 
 
-	elif i == '--rdnsr':
-		try:
-			host = argv[(argv.index('--rdnsr')+1)]
-		except IndexError:
-			print('Netkit: missing arguments')
-			exit()
-		from modules.modules import dns_reverse_resolver
-		try:
-			dns_reverse_resolver(host)
-		except NameError:
-			pass
+elif '--rdnsr' in argv:
+	try:
+		host = argv[(argv.index('--rdnsr')+1)]
+	except IndexError:
+		print('Netkit: missing arguments')
+		exit()
+	from modules.modules import dns_reverse_resolver
+	try:
+		dns_reverse_resolver(host)
+	except NameError:
+		pass
 
 
 ################################################################################
